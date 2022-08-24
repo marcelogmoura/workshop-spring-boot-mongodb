99077253460f7464d12aa1812385a9c1f9646985
Marcelo Moura <mgmoura@gmail.com>
2022-08-24 11:51:36 -0300

    Obtendo usuario pelo id

--- src/main/java/com/mgmoura/workshopmongo/services/UserService.java
+++ src/main/java/com/mgmoura/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mgmoura.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mgmoura.workshopmongo.domain.User;
 import com.mgmoura.workshopmongo.repository.UserRepository;
+import com.mgmoura.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,8 +17,18 @@ public class UserService {
 	private UserRepository repo;
 	
 	public List<User> findAll(){
-		return repo.findAll();
-		
+		return repo.findAll();		
+	}
+	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	
 	}
 
 }
+
+
+
+
