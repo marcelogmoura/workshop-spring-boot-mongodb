abeade9eea51f5ab83a7a7a9a39b3a36e0bb826d
Marcelo Moura <mgmoura@gmail.com>
2022-08-25 15:28:13 -0300

    Obtendo Post por id

---
+++ src/main/java/com/mgmoura/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.mgmoura.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mgmoura.workshopmongo.domain.Post;
+import com.mgmoura.workshopmongo.repository.PostRepository;
+import com.mgmoura.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+
+
+}
