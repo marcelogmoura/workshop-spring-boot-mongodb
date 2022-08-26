3b32ada198e43cdddfb2af7795ed4bbdff0866b7
Marcelo Moura <mgmoura@gmail.com>
2022-08-26 11:01:11 -0300

    Consulta simples com @Query

--- src/main/java/com/mgmoura/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mgmoura/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,19 @@ package com.mgmoura.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mgmoura.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+	// https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+	@Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+	List<Post> serachTitle(String text);
+		
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
