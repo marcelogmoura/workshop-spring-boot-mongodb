c0b170ac715b54f4bfffd60213b9041c1883dcd1
Marcelo Moura <mgmoura@gmail.com>
2022-08-26 11:41:03 -0300

    Consulta com varios criterios

--- src/main/java/com/mgmoura/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mgmoura/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.mgmoura.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,8 +16,12 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	// https://www.mongodb.com/docs/manual/reference/operator/query/regex/
 	@Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
 	List<Post> serachTitle(String text);
-		
-	
+			
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// https://www.mongodb.com/docs/manual/reference/operator/query/and/
+	// https://www.mongodb.com/docs/manual/reference/operator/query/gte/#mongodb-query-op.-gte
+	@Query(" { $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0 , $options: 'i' } }, { 'body': { $regex: ?0 , $options: 'i' } }, { 'comments.text': { $regex: ?0 , $options: 'i' } } ] } ] } ")
+	List<Post> fullSearch(String text , Date minDate , Date maxDate);
 
 }
